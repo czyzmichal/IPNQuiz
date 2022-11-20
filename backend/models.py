486351a66f59e860858d7c83a002956da0af7b23
hjmn6m <wiktor.bozek@aptiv.com>
2022-11-20 01:12:18 +0100

    provide endpoints, complete fastapi setup and postgres docker

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,30 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ARRAY
 from sqlalchemy.types import Date
 from database import Base
 
 
-class Record(Base):
-    __tablename__ = "Records"
+class Quiz(Base):
+    __tablename__ = "quiz"
 
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(Date)
-    country = Column(String(255), index=True)
-    cases = Column(Integer)
-    deaths = Column(Integer)
-    recoveries = Column(Integer)
+    name = Column(String(80), index=True)
+    date_created = Column(Date)
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String(255))
+    correct_answer = Column(String(255))
+    incorrect_answers = Column(ARRAY(String))
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50))
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    password = Column(String(30))
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,14 +1,33 @@
 from datetime import date
 from pydantic import BaseModel
+from typing import List
 
 
-class Record(BaseModel):
+class Quiz(BaseModel):
     id: int
+    name: str
     date: date
-    country: str
-    cases: int
-    deaths: int
-    recoveries: int
+
+    class Config:
+        orm_mode = True
+
+
+class Question(BaseModel):
+    id: int
+    question: str
+    correct_answer: str
+    incorrect_answers: List[str]
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    first_name: str
+    last_name: str
+    password: str
 
     class Config:
         orm_mode = True
